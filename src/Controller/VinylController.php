b6b1e242afd03c47120faaa1ee4578ad90c6e18c
JoaquinAyG <joaquin.ayllon@educa.madrid.org>
2022-11-03 22:46:34 +0100

    routes testing

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,7 +2,26 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
+
 class VinylController
 {
+    #[Route('/')]
+    public function homePage(): Response
+    {
+        return new Response('Hello World!');
+    }
 
+    #[Route('/search/{slug}')]
+    public function search(string $slug = null): Response
+    {
+        if ($slug) {
+            $title = 'Genre: ' . u(str_replace('-', ' ', $slug))->title(true);
+        } else {
+            $title = 'All Genres';
+        }
+        return new Response($title);
+    }
 }

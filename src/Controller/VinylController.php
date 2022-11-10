2f10c71f1d3d5de690d2c0020c471a1a325e2458
JoaquinAyG <joaquin.ayllon@educa.madrid.org>
2022-11-10 21:49:19 +0100

    added twig and gitignore

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,16 +2,30 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class VinylController
+class VinylController extends AbstractController
 {
     #[Route('/')]
     public function homePage(): Response
     {
-        return new Response('Hello World!');
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise','artist' => 'Coolio'],
+            ['song' => 'Waterfall','artist' => 'TCL'],
+            ['song' => 'Creep','artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose','artist' => 'Seal'],
+            ['song' => 'On Bended Knee','artist' => 'Boyz II Men'],
+        ];
+
+        return $this->render('vinyl/homepage.html.twig',
+            [
+                'title' => 'Home',
+                'tracks' => $tracks
+            ]
+        );
     }
 
     #[Route('/search/{slug}')]
